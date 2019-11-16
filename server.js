608cf7e8c30a710fac94b92a1c7515b757b2fec9
John Azzaro <jazzaro@temple.edu>
2019-11-15 21:34:53 -0800

    tested connection (works)

--- server.js
+++ server.js
@@ -3,3 +3,14 @@
 const express = require('express');
 const app = express();
 
+
+
+app.get('/', function(req, res) {
+    res.send('this test works!');
+});
+
+
+let server;
+server = app.listen('3000', function() {
+    console.log('Your app is listening on port 3000...');
+});
